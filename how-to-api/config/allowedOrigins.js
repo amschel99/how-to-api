b282d1702db5bbd1d7792ed5ccc6286b0ed53639
amschell <100097727+amschell@users.noreply.github.com>
2022-07-05 15:03:58 +0300

    added cors for vercel

--- how-to-api/config/allowedOrigins.js
+++ how-to-api/config/allowedOrigins.js
@@ -1,9 +1,10 @@
-const allowedOrigins=[
+const allowedOrigins = [
 
-"http://localhost:3000",
-"https://jyd-shoppers.netlify.app"
+    "http://localhost:3000",
+    "https://jyd-shoppers.netlify.app",
+    "https://how-to-frontend.vercel.app"
 
 
 
 ]
-module.exports= allowedOrigins
+module.exports = allowedOrigins
