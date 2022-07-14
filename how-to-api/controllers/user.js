132ff30049fbe2af435d1f021a68512487522601
amschell <100097727+amschell@users.noreply.github.com>
2022-07-14 13:55:55 +0300

    still more changes

--- how-to-api/controllers/user.js
+++ how-to-api/controllers/user.js
@@ -5,7 +5,7 @@ const getUser= async (req, res)=>{
     try{
         user=req.user
      const userExists=  await userModel.find({_id:user})
-   return  res.status(200).json({message:"success", data:userExists})
+   return  res.status(200).json({message:"success", data:userExists[0].name})
 
     }
     catch(error){
